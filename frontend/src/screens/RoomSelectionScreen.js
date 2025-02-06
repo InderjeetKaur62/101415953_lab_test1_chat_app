ffe7ec0420ded9855cbc5c52ee78d6d50c759770
InderjeetKaur62 <101415953@georgebrown.ca>
2025-02-05 19:58:18 -0500

    LabTest

---
+++ frontend/src/screens/RoomSelectionScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const RoomSelectionScreen = () => {
+  return (
+    <View>
+      <Text>Room Selection Screen</Text>
+    </View>
+  );
+};
+
+export default RoomSelectionScreen;
